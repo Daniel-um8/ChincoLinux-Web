1cfe8b816b33be11e44dcfa7f024a862768620aa
Daniel-um8 <danielugalde.m1993@gmail.com>
2025-08-11 21:28:36 -0400

    Third Commit

---
+++ sitio/static/sitio/js/script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((e) => {
+            if (e.isIntersecting) {
+                e.target.classList.add('in-view')
+                observer.unobserve(e.target)
+            }
+        })
+    },
+    { threshold: 0.15 }
+)
+
+document.querySelectorAll('.reveal').forEach((el) => observer.observe(el))
